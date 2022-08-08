7d8e6a9a603fec00091ec4623d3337c1a76bf1f0
zaki178 <74228332+zaki178@users.noreply.github.com>
2022-08-08 08:55:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,23 +25,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "✈ᵇᵒᵗ𝒈𝒊𝒓𝒍✔-Mdོ" //namabot kalian
+global.ownername= "ྂᴋᴀɴɢ ᴋᴏᴍᴘᴏʀ🍓⃝﷽" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
+global.youtube = "https://www.instagram.com/ry.zki07?igshid=YmMyMTA2M2Y=" //bebas asal jan hapus
+global.myweb2 = "https://linktr.ee/" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['62895376867500@','6282180353187','62895376867500','6282180353187@s.whatsapp.net'] //premium user
+global.owner = ["62895376867500","622150996855"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "62895376867500" // nomor wa kalian
+global.ownernomerr = "+62895376867500" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.author = 'Di Buat Oleh Zaki' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
